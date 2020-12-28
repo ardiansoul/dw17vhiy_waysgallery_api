1e7c7b2804e6df430b44e273725db52068a68323
ardiansoul <yanaardi523@gmail.com>
2020-12-28 15:33:55 +0700

    deploy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 // const Seed = require("./seeders/index");
 
 // const db = require("./models");
